eed0079fca08ecb24f458490c0982358f620af89
Trever Shick <trever.shick@tanium.com>
2017-12-25 23:31:18 -0500

    start adding tests

--- src/djs/config.rs
+++ src/djs/config.rs
@@ -2,6 +2,7 @@
 use djs::defaults::*;
 use std::fs;
 use std;
+use std::path::Path;
 use std::rc::Rc;
 use std::cell::RefCell;
 use std::fmt::{Display, Formatter};
@@ -39,13 +40,15 @@ impl<T: Clone> ConfigValue<T> {
         ConfigValue { value: value, source: source }
     }
 
-    pub fn set(&mut self, value : T, source: String) {
+    pub fn set<S>(&mut self, value: T, source: S)  where S: Into<String> {
         self.value = value;
-        self.source = source;
+        self.source = source.into();
     }
+
     pub fn get(&self) -> T {
         self.value.clone()
     }
+
     pub fn source(&self) -> String {
         self.source.clone()
     }
@@ -66,22 +69,46 @@ impl Config {
         }
     }
 
+    fn branch_name_contains_project(&self) -> bool {
+        self.branch.get().to_uppercase().as_str().contains(self.project.get().to_uppercase().as_str())
+    }
+
+    fn abbreviated_build(&self) -> String {
+        match self.build.get().trim() {
+            "lastSuccessfulBuild" => "ls".to_string(),
+            "lastKeepForever" => "kf".to_string(),
+            other => other.to_string()
+        }
+    }
+
     pub fn destination_path(&self) -> String {
         // if the destination is a dir, then we build it
         // if the destination is a filename return it
-        let default_dest = format!("{destination}/{branch}-{build}-{project}.xml",
-                destination = self.destination.get(),
-                branch = self.branch.get(),
-                build = self.build.get(),
-                project = self.project.get());
-
         let dest_is_dir = match fs::metadata(self.destination.get()) {
             Ok(meta) => meta.is_dir() && !meta.permissions().readonly(),
             Err(_) => false
         };
 
         if dest_is_dir {
-            default_dest
+            let mut d = self.destination.get().clone();
+            d.push('/');
+
+            if !self.branch_name_contains_project() {
+                d.push_str(self.project.get().to_lowercase().as_ref());
+                d.push('-');
+            }
+
+            d.push_str(self.branch.get().to_lowercase().as_ref());
+            d.push('-');
+
+            d.push_str(self.abbreviated_build().to_lowercase().as_ref());
+
+
+            if let Some(ext) = Path::new(self.solution.get().as_str()).extension() {
+                d.push('.');
+                d.push_str(ext.to_str().unwrap());
+            }
+            d
         } else {
             self.destination.get()
         }
@@ -114,6 +141,56 @@ pub fn validate_config(config : Rc<RefCell<Config>>) -> Result<(), String> {
     if c.is_destination_a_dir() && !c.is_destination_writable() {
         return Err(format!("The destination directory {} is not writable.", c.destination.get()));
     }
+    // TODO - lots more validation here, URL formats,etc...
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    macro_rules! cset {
+        ($config: ident, $opt:ident, $val: expr) => {
+            $config.$opt.set($val.to_string(), "test");
+        }
+    }
+
+    mod abbreviated_build {
+        use super::super::*;
+
+        #[test]
+        fn last_successful() {
+            let mut c = Config { ..Default::default() };
+            cset!(c, build, "lastSuccessfulBuild");
+            assert_eq!("ls", c.abbreviated_build());
+        }
+        #[test]
+        fn last_keep_forever() {
+            let mut c = Config { ..Default::default() };
+            cset!(c, build, "lastKeepForever");
+            assert_eq!("kf", c.abbreviated_build());
+        }
+        #[test]
+        fn other() {
+            let mut c = Config { ..Default::default() };
+            cset!(c, build, "master");
+            assert_eq!("master", c.abbreviated_build());
+        }
+    }
+
+    mod destination_path {
+        use super::super::*;
+
+        #[test]
+        fn default_format() {
+            let c = Config { ..Default::default() };
+            assert_eq!(c.destination_path().as_str(), "./discover-master-ls.xml");
+        }
+
+        #[test]
+        fn doesnt_duplicate_project() {
+            let mut c = Config { ..Default::default() };
+            cset!(c,branch, "DISCOVER-1814");
+            assert_eq!(c.destination_path().as_str(), "./discover-1814-ls.xml");
+        }
+    }
 }
