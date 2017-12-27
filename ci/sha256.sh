8ee7635dd6d1da68e46a30be0af7bcfb0ca19592
Trever Shick <trever.shick@tanium.com>
2017-12-27 18:35:22 -0500

    Change to sha256.sh to work on mac

--- ci/sha256.sh
+++ ci/sha256.sh
@@ -8,18 +8,28 @@ if [ $# != 1 ]; then
 fi
 version="$1"
 
+shasum_cmd="sha256sum"
+
+if [ "$(uname)" = "Darwin" ]; then
+  shasum_cmd="shasum -a 256"
+fi
+
 # Linux and Darwin builds.
 for arch in i686 x86_64; do
-  for target in apple-darwin unknown-linux-musl; do
+  for target in apple-darwin unknown-linux-musl unknown-linux-gnu; do
     url="https://github.com/trevershick/djs/releases/download/$version/djs-$version-$arch-$target.tar.gz"
-    sha=$(curl -sfSL "$url" | sha256sum)
-    echo "$version-$arch-$target $sha"
+    if curl --output /dev/null --silent --head --fail "$url"; then
+      sha=$(curl -sfSL "$url" | $shasum_cmd)
+      echo "$version-$arch-$target $sha"
+    fi
   done
 done
 
 # Source.
 for ext in zip tar.gz; do
   url="https://github.com/trevershick/djs/archive/$version.$ext"
-  sha=$(curl -sfSL "$url" | sha256sum)
-  echo "source.$ext $sha"
+  if curl --output /dev/null --silent --head --fail "$url"; then
+    sha=$(curl -sfSL "$url" | $shasum_cmd)
+    echo "source.$ext $sha"
+  fi
 done
