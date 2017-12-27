c4746788803fd8ec359d720725b584fd34eca80c
Trever Shick <trever.shick@tanium.com>
2017-12-27 18:35:45 -0500

    Update 0.1.0 sha256 values

--- pkg/brew/djs-bin.rb
+++ pkg/brew/djs-bin.rb
@@ -5,10 +5,10 @@ class DjsBin < Formula
 
   if OS.mac?
       url "https://github.com/trevershick/djs/releases/download/#{version}/djs-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "ee670b0fba46323ee9a2d1c5b8bee46fa3e45778f6f105f2e8e9ee29e8bd0d45"
+      sha256 "195e8fa0a93060f166d7aaa0411c48f4fa65ccba5241a44ed95dfe8683f208e3"
   elsif OS.linux?
-      url "https://github.com/trevershick/djs/releases/download/#{version}/djs-#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "ac595c2239b9a30e0e0744578afa6b73e32cdd8ae61d4f1c0ee5d6b55adbadcf"
+      url "https://github.com/trevershick/djs/releases/download/#{version}/djs-#{version}-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "32c5031f6ea17ff5e694518f945e3fcde0cbd2832a4d1da4fa6d6a183ec7635b"
   end
 
   conflicts_with "djs"
