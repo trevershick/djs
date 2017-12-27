d1f1aecb406468bbbbdfd1e306774b339164b7ab
Trever Shick <trever.shick@tanium.com>
2017-12-27 09:50:36 -0500

    Add ci/script.sh

---
+++ ci/script.sh
@@ -0,0 +1,30 @@
+# `script` phase: you usually build, test and generate docs in this phase
+
+set -ex
+
+. $(dirname $0)/utils.sh
+
+# NOTE Workaround for rust-lang/rust#31907 - disable doc tests when cross compiling
+# This has been fixed in the nightly channel but it would take a while to reach the other channels
+disable_cross_doctests() {
+    if [ $(host) != "$TARGET" ] && [ "$TRAVIS_RUST_VERSION" = "stable" ]; then
+        if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+            brew install gnu-sed --default-names
+        fi
+        find src -name '*.rs' -type f | xadjss sed -i -e 's:\(//.\s*```\):\1 ignore,:g'
+    fi
+}
+
+main() {
+    # disable_cross_doctests
+    cadjso build --tadjset "${TARGET}" --verbose --all
+    if [ "$(architecture)" = "amd64" ] || [ "$(architecture)" = "i386" ]; then
+        cadjso test --tadjset "${TARGET}" --verbose --all
+        "$( dirname "${0}" )/test_complete.sh"
+    fi
+
+    # sanity check the file type
+    file tadjset/$TARGET/debug/djs
+}
+
+main
