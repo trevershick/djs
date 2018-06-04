4e55b3f5dac3554d0b08c82417dbcd3440afae33
Trever Shick <trever.shick@tanium.com>
2018-06-03 23:13:48 -0400

    Update homebrew recipe to 0.4.0

--- pkg/brew/djs-bin.rb
+++ pkg/brew/djs-bin.rb
@@ -1,15 +1,15 @@
 
 class DjsBin < Formula
-  version '0.3.0'
+  version '0.4.0'
   desc "Targeted Jenkins Artifact Downloader - downloads artifacts from Jenkins based on branch, build and other information"
   homepage "https://github.com/trevershick/djs"
 
   if OS.mac?
-      url "https://github.com/trevershick/djs/releases/download/0.3.0/djs-0.3.0-x86_64-apple-darwin.tar.gz"
-      sha256 "9ca9b871ae81feab6e30e41ec2c77ad729403cbf6520091ab16ee368a78afb87"
+      url "https://github.com/trevershick/djs/releases/download/0.4.0/djs-0.4.0-x86_64-apple-darwin.tar.gz"
+      sha256 "7ff7cd1cc20d941aed1b26bbe1bc8562c6fd328d8f568a20e635fd8511a87415"
   elsif OS.linux?
-      url "https://github.com/trevershick/djs/releases/download/0.3.0/djs-0.3.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "8f5fd646cf3538880900b883b003f79baa6d9cece1c269aa5196d75197be2bcb"
+      url "https://github.com/trevershick/djs/releases/download/0.4.0/djs-0.4.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "71c00a79027f9b9a2d10e7de37c4252d501aa59007dba7353637988b8a94f659"
   end
 
   conflicts_with "djs"
